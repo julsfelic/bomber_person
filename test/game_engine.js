dd841fc7fc3f0cbfdb167a24b499a8e806f9e851
Julian Feliciano <julsfelic@gmail.com>
2016-05-17 13:25:41 -0600

    Create GameEngine
    * Game engine has a map an bomber person
    * Game engine can move a bomber person
    * Game engine checks to see if bomber person can move

--- test/game_engine.js
+++ test/game_engine.js
@@ -1,1 +1,36 @@
-const expect = require('mocha').expect
+const expect = require('chai').expect;
+const GameEngine = require('../lib/game_engine');
+const Person = require('../lib/person');
+const Map = require('../lib/map');
+
+describe('GameEngine', function() {
+  it('is instantiated', function() {
+    let person = new Person([0,0]);
+    let map = new Map(300, 500);
+    let gameEngine = new GameEngine(person, map);
+
+    expect(gameEngine.map.width).to.eq(500);
+    expect(gameEngine.person.x).to.eq(0);
+  });
+
+  it('can move player forward', function() {
+    let person = new Person([0,0]);
+    let map = new Map(300, 500);
+    let gameEngine = new GameEngine(person, map);
+
+    gameEngine.move(1,0);
+
+    expect(gameEngine.person.x).to.eq(1);
+  });
+
+  it('cannot move player into a pillar', function() {
+    let person = new Person([0,0]);
+    let map = new Map(300, 500);
+    let gameEngine = new GameEngine(person, map);
+
+    gameEngine.move(1,1);
+
+    expect(gameEngine.person.x).to.eq(0);
+    expect(gameEngine.person.y).to.eq(0);
+  });
+});
