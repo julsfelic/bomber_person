a79ce35c58ce8bca6e1b53696153bd978d22a274
Julian Feliciano <julsfelic@gmail.com>
2016-05-17 13:06:35 -0600

    Add game engine and game engine test file

--- test/person.js
+++ test/person.js
@@ -1,20 +1,17 @@
 const expect = require('chai').expect;
 const Person = require('../lib/person');
-const Map = require('../lib/map');
 
 describe('Person', function() {
 
 
   it('has a location', function() {
-    let map = new Map(300, 500);
-    let person = new Person([0, 0], map);
+    let person = new Person([0, 0]);
     expect(person.x).to.eq(0);
     expect(person.y).to.eq(0);
   });
 
   it('can move', function () {
-    let map = new Map(300, 500);
-    let person = new Person([0, 0], map);
+    let person = new Person([0, 0]);
     person.move(1, 0);
     expect(person.x).to.eq(1);
     expect(person.y).to.eq(0);
