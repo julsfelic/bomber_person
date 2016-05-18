e156f26f349c833c970d818b8acf7347906e452a
Julian Feliciano <julsfelic@gmail.com>
2016-05-18 12:05:46 -0600

    Add sinon testing framework and clean up existing tests

--- test/controller.js
+++ test/controller.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect
+
 const Controller = require('../lib/controller');
 const Person = require('../lib/person');
 const Map = require('../lib/map');
@@ -6,9 +7,9 @@ const GameEngine = require('../lib/game_engine');
 
 describe('Controller', function() {
   it('can be instantiated', function() {
-    let playerOne = new Person(1, 0, 0);
-    let playerTwo = new Person(2, 12, 10);
-    let people = {1: playerOne, 2: playerTwo};
+    let playerOne = new Person({id: 1, x: 0, y: 0, image: 'test.png'});
+    let playerTwo = new Person({id: 2, x: 12, y: 10, image: 'test.png'});
+    let people = { 1: playerOne, 2: playerTwo };
     let map = new Map(300, 500);
     let gameEngine = new GameEngine(people, map);
     let controller = new Controller(1, {
